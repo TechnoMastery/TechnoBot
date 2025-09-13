2b3d1e1c97832fe1bb07f5cb25e414505ef00aaa
Minheur2000 <adrien.rousselet@hotmail.com>
2025-09-13 16:41:48 +0200

    working on ""/verify_mc_player"...

--- cogs/commands.py
+++ cogs/commands.py
@@ -12,20 +12,20 @@ class CommandsCog(commands.Cog):
         await interaction.response.send_message("Hello World!")
 
     @app_commands.command(name="confirm_player", description="Permet de donner la listes des infos a un joueur.")
-    async def verify_mc_player(self, interaction: discord.Interaction, pseudo: str, target: discord.Member):
+    async def verify_mc_player(self, interaction: discord.Interaction, pseudo: str, target: discord.User):
         embed = discord.Embed(
             title=f"Salut {target.mention}",
             description=f"> → IP: `outerlands.fr:30001`\n"
                         f"> → Modpack: [TechnoMastery](https://www.curseforge.com/minecraft/modpacks/techno-mastery)\n"
                         f"> → Version de modpack : *v8.5*\n"
-                        f"> Rôle <1342104059234222160> donné à tout les membres\n"
-                        f"> → Règlement dans <1248891010965045328>\n\n"
+                        f"> Rôle <@&1342104059234222160> donné à tout les membres\n"
+                        f"> → Règlement dans <#1248891010965045328>\n\n"
                         f"**Ton pseudo ingame** :`{pseudo}`\n",
             color=discord.Color.blue()
         )
         embed.set_footer(text="Si votre pseudo est incorrect, merci de le signaler !", icon_url=target.avatar.url)
 
-        await interaction.response.send_message(embed=embed)
+        await interaction.response.send_message(embed=embed, allowed_mentions=discord.AllowedMentions.all())
     # use "interaction.followup.send(TEXT)" to send "TEXT" as a followup (as only one answer allowed)
     # use "interaction.response.defer()" to tell discord the bot is thinking. Then, use a followup to answer.
 
